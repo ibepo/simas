8ade345fecb19a070daa3df837c105d84db6b212
Shuzhi Cai <Shuzhi Cai@192.168.123.26>
2015-04-22 17:15:12 +0800

    添加九宫格模块

---
+++ src/com/simas/ui/MainAct.java
@@ -0,0 +1,16 @@
+package com.simas.ui;
+
+import android.os.Bundle;
+
+import com.simas.R;
+import com.simas.base.BaseAct;
+
+public class MainAct extends BaseAct {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main_jiugongge);
+	}
+
+}
