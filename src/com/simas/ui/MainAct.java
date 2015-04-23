f884c17e696c0ce49ab46dfbded75b902f0743d8
Shuzhi Cai <Shuzhi Cai@192.168.123.26>
2015-04-23 08:52:31 +0800

    hell

--- src/com/simas/ui/MainAct.java
+++ src/com/simas/ui/MainAct.java
@@ -2,15 +2,25 @@ package com.simas.ui;
 
 import android.os.Bundle;
 
+import com.finddreams.graygridView.MyGridAdapter;
+import com.finddreams.graygridView.MyGridView;
 import com.simas.R;
 import com.simas.base.BaseAct;
 
 public class MainAct extends BaseAct {
+	private MyGridView gridview;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_jiugongge);
+		initView();
+	}
+
+	private void initView() {
+		gridview = (MyGridView) findViewById(R.id.gridview);
+		gridview.setAdapter(new MyGridAdapter(this));
+
 	}
 
 }
