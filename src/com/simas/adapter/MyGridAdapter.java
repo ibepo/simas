64887995ee2604031eed7e217fa2f9520c2a139b
Shuzhi Cai <Shuzhi Cai@192.168.123.26>
2015-04-23 09:56:39 +0800

    功能

--- src/com/simas/adapter/MyGridAdapter.java
+++ src/com/simas/adapter/MyGridAdapter.java
@@ -1,23 +1,26 @@
 package com.simas.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.finddreams.graygridView.BaseViewHolder;
 import com.simas.R;
+import com.simas.ui.testsuperlistview;
 
 public class MyGridAdapter extends BaseAdapter {
 	private Context mContext;
 
-	public String[] img_text = { "时间管理", "基础信息", "信息共享", "巡查轨迹", "统计分析", "民情日志", "居家养老" };
-	public int[] imgs = { R.drawable.icon_dark_save, R.drawable.icon_dark_save, R.drawable.icon_dark_save,
-			R.drawable.icon_dark_save, R.drawable.icon_dark_save, R.drawable.icon_dark_save, R.drawable.icon_dark_save,
-			R.drawable.icon_dark_save, R.drawable.icon_dark_save };
+	public String[] img_text = { "事件管理", "基础信息", "信息共享", "巡查轨迹", "统计分析", "民情日志", "居家养老" };
+	public int[] imgs = { R.drawable.home_event, R.drawable.home_jichuxinxi, R.drawable.home_xinxigongxiang,
+			R.drawable.home_xunchaguiji, R.drawable.home_tongjifenxi, R.drawable.home_mingqingrizhi,
+			R.drawable.home_jujiayanglao, };
 
 	public MyGridAdapter(Context mContext) {
 		super();
@@ -40,16 +43,31 @@ public class MyGridAdapter extends BaseAdapter {
 	}
 
 	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
+	public View getView(final int position, View convertView, ViewGroup parent) {
 		if (convertView == null) {
 			convertView = LayoutInflater.from(mContext).inflate(R.layout.grid_item, parent, false);
 		}
 		TextView tv = BaseViewHolder.get(convertView, R.id.tv_item);
 		ImageView iv = BaseViewHolder.get(convertView, R.id.iv_item);
 		iv.setBackgroundResource(imgs[position]);
-
 		tv.setText(img_text[position]);
+		convertView.setTag(position);
+
+		convertView.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				switch (position) {
+				case 0:
+					Intent mainIntent = new Intent(mContext, testsuperlistview.class);
+					mContext.startActivity(mainIntent);
+					break;
+
+				default:
+					break;
+				}
+
+			}
+		});
 		return convertView;
 	}
-
 }
